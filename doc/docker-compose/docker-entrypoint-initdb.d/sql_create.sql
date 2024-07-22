d877b4793dcfdde00320449ca6cc371916dd64be
André <andre@tellesribeiro.net>
2024-07-22 23:53:30 +0100

    docker compose to simplify testing

---
+++ doc/docker-compose/docker-entrypoint-initdb.d/sql_create.sql
@@ -0,0 +1,32 @@
+\c domaintool;
+
+CREATE TABLE domain_info (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    registrar VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    tier VARCHAR(255) NOT NULL,
+    transfer_to VARCHAR(255),
+    last_check TIMESTAMP NOT NULL,
+    spf VARCHAR(255),
+    dmarc VARCHAR(255),
+    nameservers TEXT,
+    status BOOLEAN
+);
+
+
+CREATE TABLE domain_info_history (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    registrar VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    tier VARCHAR(255) NOT NULL,
+    transfer_to VARCHAR(255),
+    last_check TIMESTAMP NOT NULL,
+    spf VARCHAR(255),
+    dmarc VARCHAR(255),
+    nameservers TEXT,
+    status BOOLEAN
+);
+
+
